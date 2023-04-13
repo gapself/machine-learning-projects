638da0fcb52d4309b8b1d144e81a6a2ff74c69f7
gapself <gabriela.paszkiewicz@gmail.com>
2023-04-13 16:32:04 +0200

    nbd renamed

--- nbd/zestaw3-mongo/zapytanie_1.js
+++
@@ -1,2 +0,0 @@
-// w folderze zestaw3-mongo: mongoimport --file ./cwiczenia2.json --db nbd --jsonArray -collection
-printjson(db.people.findOne())
---
+++ non-relational-databases/zestaw3-mongo/zapytanie_1.js
@@ -0,0 +1,2 @@
+// w folderze zestaw3-mongo: mongoimport --file ./cwiczenia2.json --db non-relational-databases --jsonArray -collection
+printjson(db.people.findOne())
